46fdcb7fbb576ba3baa3d0a34f243dd6349c14c6
Light it or Pass it <niffoisme@gmail.com>
2025-10-19 09:09:07 +0100

    Add reverse iterator on fox::list

--- PixelFoxCore/include/core/list.h
+++ PixelFoxCore/include/core/list.h
@@ -31,6 +31,14 @@ namespace fox
 		node*		m_pHead{ nullptr };
 		std::size_t m_nSize{	0	 };
 
+		_NODISCARD node* tail_node() const noexcept
+		{
+			node* t = m_pHead;
+			if (!t) return nullptr;
+			while (t->next) { t = t->next; }
+			return t;
+		}
+
 	public:
 		list() noexcept  = default;
 
@@ -120,6 +128,18 @@ namespace fox
 			}
 			m_pHead = nullptr;
 			m_nSize = 0;
+		}
+
+		_Ret_notnull_ T& front() noexcept
+		{
+			assert(m_pHead && "list::front() called on empty list");
+			return m_pHead->value;
+		}
+
+		_Ret_notnull_ const T& front() const noexcept
+		{
+			assert(m_pHead && "list::front() called on empty list");
+			return m_pHead->value;
 		}
 
 		void push_front(_In_ const T& v) 
@@ -216,6 +236,7 @@ namespace fox
 			friend bool operator!=(_In_ const const_iterator& a, _In_ const const_iterator& b) noexcept { return a.n != b.n; }
 		};
 
+		//~ front access
 		_NODISCARD iterator		  begin ()		 noexcept { return iterator(m_pHead);		}
 		_NODISCARD iterator		  end   ()		 noexcept { return iterator(nullptr);		}
 		_NODISCARD const_iterator begin () const noexcept { return const_iterator(m_pHead); }
@@ -223,16 +244,86 @@ namespace fox
 		_NODISCARD const_iterator cbegin() const noexcept { return const_iterator(m_pHead); }
 		_NODISCARD const_iterator cend  () const noexcept { return const_iterator(nullptr); }
 
-		_Ret_notnull_ T& front() noexcept
+		class const_reverse_iterator;
+		class reverse_iterator
 		{
-			assert(m_pHead && "list::front() called on empty list");
-			return m_pHead->value;
-		}
+			friend class const_reverse_iterator;
+			node* m_head{ nullptr };
+			node* m_cur { nullptr };
+		public:
+			using iterator_category = std::forward_iterator_tag; // only supports ++
+			using value_type		= T;
+			using difference_type	= std::ptrdiff_t;
+			using pointer			= T*;
+			using reference			= T&;
 
-		_Ret_notnull_ const T& front() const noexcept
+			reverse_iterator() noexcept = default;
+			reverse_iterator(_In_opt_ node* head, _In_opt_ node* cur) noexcept : m_head(head), m_cur(cur) {}
+
+			reference operator*()  const noexcept { return m_cur->value; }
+			pointer   operator->() const noexcept { return &m_cur->value; }
+
+			reverse_iterator& operator++() noexcept
+			{
+				if (!m_cur) { return *this; }
+				if (m_cur == m_head) { m_cur = nullptr; return *this; }
+				
+				node* p = m_head;
+				while (p && p->next != m_cur) { p = p->next; }
+				
+				m_cur = p;
+				
+				return *this;
+			}
+			reverse_iterator operator++(int) noexcept { reverse_iterator t = *this; ++(*this); return t; }
+
+			friend bool operator==(_In_ const reverse_iterator& a, _In_ const reverse_iterator& b) noexcept { return a.m_cur == b.m_cur; }
+			friend bool operator!=(_In_ const reverse_iterator& a, _In_ const reverse_iterator& b) noexcept { return a.m_cur != b.m_cur; }
+		};
+
+		class const_reverse_iterator
 		{
-			assert(m_pHead && "list::front() called on empty list");
-			return m_pHead->value;
-		}
+			const node* m_head{ nullptr };
+			const node* m_cur { nullptr };
+		public:
+			using iterator_category = std::forward_iterator_tag;
+			using value_type		= const T;
+			using difference_type	= std::ptrdiff_t;
+			using pointer			= const T*;
+			using reference			= const T&;
+
+			const_reverse_iterator() noexcept = default;
+			const_reverse_iterator(_In_opt_ const node* head, _In_opt_ const node* cur) noexcept : m_head(head), m_cur(cur) {}
+			const_reverse_iterator(_In_ const reverse_iterator& it) noexcept : m_head(it.m_head), m_cur(it.m_cur)
+			{}
+
+			reference operator* () const noexcept { return m_cur->value; }
+			pointer   operator->() const noexcept { return &m_cur->value; }
+
+			const_reverse_iterator& operator++() noexcept
+			{
+				if (!m_cur) { return *this; }
+				if (m_cur == m_head) { m_cur = nullptr; return *this; }
+				
+				const node* p = m_head;
+				while (p && p->next != m_cur) { p = p->next; }
+				
+				m_cur = p;
+				
+				return *this;
+			}
+			const_reverse_iterator operator++(int) noexcept { const_reverse_iterator t = *this; ++(*this); return t; }
+
+			friend bool operator==(_In_ const const_reverse_iterator& a, _In_ const const_reverse_iterator& b) noexcept { return a.m_cur == b.m_cur; }
+			friend bool operator!=(_In_ const const_reverse_iterator& a, _In_ const const_reverse_iterator& b) noexcept { return a.m_cur != b.m_cur; }
+		};
+
+		//~ Reverse access
+		_NODISCARD reverse_iterator rbegin()			  noexcept { return reverse_iterator(m_pHead, tail_node());		  }
+		_NODISCARD reverse_iterator rend()				  noexcept { return reverse_iterator(m_pHead, nullptr);			  }
+		_NODISCARD const_reverse_iterator rbegin()  const noexcept { return const_reverse_iterator(m_pHead, tail_node()); }
+		_NODISCARD const_reverse_iterator rend()    const noexcept { return const_reverse_iterator(m_pHead, nullptr);	  }
+		_NODISCARD const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(m_pHead, tail_node()); }
+		_NODISCARD const_reverse_iterator crend()   const noexcept { return const_reverse_iterator(m_pHead, nullptr);	  }
 	};
 }
